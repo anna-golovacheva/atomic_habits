790a184ff44d73bedbc16025b84115ae78326b02
anna_g <anna.ch.ollala@gmail.com>
2023-11-12 12:34:32 +0300

    refactor

--- config/celery.py
+++ config/celery.py
@@ -1,6 +1,6 @@
 import os
-
 from celery import Celery
+
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
@@ -9,9 +9,3 @@ app = Celery('config')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
 app.autodiscover_tasks()
-
-
-# @app.task(bind=True)
-# def debug_task(self):
-#     print(f'Request: {self.request!r}')
-
